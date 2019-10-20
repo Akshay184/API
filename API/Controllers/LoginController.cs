c70529643bfe1302f3813684348345bb21c2fdde
akshay184 <akshayagarwal184@gmail.com>
2019-10-20 19:39:11 +0530

    authentication using jwt token

---
+++ API/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API.Models;
+
+namespace API.Controllers
+{
+    public class LoginController : ApiController
+    {
+
+        [HttpGet]
+        public HttpResponseMessage Validate(string token, int id)
+        {
+            bool exists = new Student().Students(id) != null;
+            if (!exists) return Request.CreateResponse(HttpStatusCode.NotFound,
+                 "The user was not found.");
+            string tokenUsername = TokenManager.ValidateToken(token);
+            if ((id.ToString()).Equals(tokenUsername))
+                return Request.CreateResponse(HttpStatusCode.OK,id);
+            return Request.CreateResponse(HttpStatusCode.BadRequest,"Bad Request");
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Login(Student st)
+        {
+            Student student = new Student();
+            var entity = student.Students(st.id);
+            if (entity == null)
+                return Request.CreateResponse(HttpStatusCode.NotFound, "The user does not exist");
+            return Request.CreateResponse(HttpStatusCode.OK, TokenManager.GenerateToken(st.id.ToString()));
+        }
+
+    }
+}
---
+++ API/Dbo/Authenication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using Microsoft.Owin.Security.Jwt;
+
+namespace API.Dbo
+{
+    public class Authenication : AuthorizationFilterAttribute
+    {
+        //public override void OnAuthorization(HttpActionContext filterContext)
+        //{
+        //    if(!IsUserAuthorized(filterContext))
+        //    {
+        //        ShowAuthenticationError(filterContext);
+        //        return;
+        //    }
+        //    base.OnAuthorization(filterContext);
+        //}
+    }
+}
